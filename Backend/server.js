6114f9f0bc3c9c1138f71fe5874f527edb7a46ec
Ramakrishna mandadi <105452720+Ramakrishna200@users.noreply.github.com>
2024-06-18 15:10:50 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,23 @@
-import express from "express"
-
-// import bodyParser from 'bodyParser';
+import express from "express";
+import cors from "cors";
 import { userrouter } from "./Routes/userroutes.js";
 import { studentrouter } from "./Routes/studentroutes.js";
 import { Interviewrouter } from "./Routes/interviewroutes.js";
-import connectDB from "./config.js"
-
-
-
+import connectDB from "./config.js";
 import { downloadrouter } from "./Routes/csvroutes.js";
-import cors from "cors"
-
-
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON bodies
 app.use(express.json());
+
+// CORS options
+const allowedOrigins = ['http://localhost:3000', 'https://serene-alfajores-5e08c1.netlify.app'];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (allowedOrigins.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
@@ -28,26 +26,19 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
-
 // Middleware to enable CORS with options
 app.use(cors(corsOptions));
 
-// Middleware
-// app.use(bodyParser.json());
-
-// // Routes
- app.use('/api/users', userrouter);
+// Routes
+app.use('/api/users', userrouter);
 app.use('/api/students', studentrouter);
-app.use('/api/interviews',Interviewrouter);
-
+app.use('/api/interviews', Interviewrouter);
 app.use('/api/csv', downloadrouter);
 
 // MongoDB connection
+connectDB();
 
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  connectDB()
-  
-
 });
